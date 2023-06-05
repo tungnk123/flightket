041e432a965c27c0da3be6dcf89e7a1e5997d155
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-05 20:26:49 +0700

    Add name and icons for Button

--- flightket/Forms-Admin/FormHomeAdmin.Designer.cs
+++ flightket/Forms-Admin/FormHomeAdmin.Designer.cs
@@ -30,7 +30,79 @@ namespace flightket.Forms_Admin
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHomeAdmin));
+            this.btn_taoNhomNguoiDung = new System.Windows.Forms.Button();
+            this.btn_quanLyTaiKhoan = new System.Windows.Forms.Button();
+            this.btn_phanQuyen = new System.Windows.Forms.Button();
+            this.btn_caiDat = new System.Windows.Forms.Button();
+            this.btn_dangXuat = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_taoNhomNguoiDung
+            // 
+            this.btn_taoNhomNguoiDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_taoNhomNguoiDung.Image = global::flightket.Properties.Resources.icons8_add_male_user_group_50;
+            this.btn_taoNhomNguoiDung.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_taoNhomNguoiDung.Location = new System.Drawing.Point(597, 323);
+            this.btn_taoNhomNguoiDung.Name = "btn_taoNhomNguoiDung";
+            this.btn_taoNhomNguoiDung.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_taoNhomNguoiDung.Size = new System.Drawing.Size(446, 100);
+            this.btn_taoNhomNguoiDung.TabIndex = 0;
+            this.btn_taoNhomNguoiDung.Text = "Tạo nhóm người dùng";
+            this.btn_taoNhomNguoiDung.UseVisualStyleBackColor = true;
+            // 
+            // btn_quanLyTaiKhoan
+            // 
+            this.btn_quanLyTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_quanLyTaiKhoan.Image = global::flightket.Properties.Resources.icons8_account_66;
+            this.btn_quanLyTaiKhoan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_quanLyTaiKhoan.Location = new System.Drawing.Point(1113, 323);
+            this.btn_quanLyTaiKhoan.Name = "btn_quanLyTaiKhoan";
+            this.btn_quanLyTaiKhoan.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_quanLyTaiKhoan.Size = new System.Drawing.Size(446, 100);
+            this.btn_quanLyTaiKhoan.TabIndex = 1;
+            this.btn_quanLyTaiKhoan.Text = "Quản lý tài khoản";
+            this.btn_quanLyTaiKhoan.UseVisualStyleBackColor = true;
+            // 
+            // btn_phanQuyen
+            // 
+            this.btn_phanQuyen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_phanQuyen.Image = global::flightket.Properties.Resources.permission;
+            this.btn_phanQuyen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_phanQuyen.Location = new System.Drawing.Point(651, 499);
+            this.btn_phanQuyen.Name = "btn_phanQuyen";
+            this.btn_phanQuyen.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_phanQuyen.Size = new System.Drawing.Size(392, 100);
+            this.btn_phanQuyen.TabIndex = 2;
+            this.btn_phanQuyen.Text = "Phân quyền";
+            this.btn_phanQuyen.UseVisualStyleBackColor = true;
+            // 
+            // btn_caiDat
+            // 
+            this.btn_caiDat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_caiDat.Image = global::flightket.Properties.Resources.icons8_settings_50;
+            this.btn_caiDat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_caiDat.Location = new System.Drawing.Point(1113, 499);
+            this.btn_caiDat.Name = "btn_caiDat";
+            this.btn_caiDat.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_caiDat.Size = new System.Drawing.Size(392, 100);
+            this.btn_caiDat.TabIndex = 3;
+            this.btn_caiDat.Text = "Cài đặt";
+            this.btn_caiDat.UseVisualStyleBackColor = true;
+            // 
+            // btn_dangXuat
+            // 
+            this.btn_dangXuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangXuat.Image = global::flightket.Properties.Resources.icons8_log_out_50;
+            this.btn_dangXuat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_dangXuat.Location = new System.Drawing.Point(1487, 834);
+            this.btn_dangXuat.Margin = new System.Windows.Forms.Padding(3, 3, 30, 30);
+            this.btn_dangXuat.Name = "btn_dangXuat";
+            this.btn_dangXuat.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_dangXuat.Size = new System.Drawing.Size(261, 103);
+            this.btn_dangXuat.TabIndex = 7;
+            this.btn_dangXuat.Text = "Đăng xuất";
+            this.btn_dangXuat.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn_dangXuat.UseVisualStyleBackColor = true;
             // 
             // FormHomeAdmin
             // 
@@ -39,6 +111,11 @@ namespace flightket.Forms_Admin
             this.BackgroundImage = global::flightket.Properties.Resources._350248398_786598096329880_2111811280162483567_n__1_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.btn_dangXuat);
+            this.Controls.Add(this.btn_caiDat);
+            this.Controls.Add(this.btn_phanQuyen);
+            this.Controls.Add(this.btn_quanLyTaiKhoan);
+            this.Controls.Add(this.btn_taoNhomNguoiDung);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormHomeAdmin";
@@ -49,5 +126,11 @@ namespace flightket.Forms_Admin
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_taoNhomNguoiDung;
+        private System.Windows.Forms.Button btn_quanLyTaiKhoan;
+        private System.Windows.Forms.Button btn_phanQuyen;
+        private System.Windows.Forms.Button btn_caiDat;
+        private System.Windows.Forms.Button btn_dangXuat;
     }
 }
