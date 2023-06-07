885f5c0605592dd92baaeb0b1d30349fb87aa4a5
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-07 11:14:34 +0700

    Add controls for FormThemNhomNguoiDung and add new role into db
    # Conflicts:
    #	flightket/Forms-Admin/FormTaoNhomNguoiDung.Designer.cs

--- flightket/Forms-Admin/FormTaoNhomNguoiDung.cs
+++ flightket/Forms-Admin/FormTaoNhomNguoiDung.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace flightket.Forms_Admin
 {
     public partial class FormTaoNhomNguoiDung : Form
     {
+        Random random = new Random();
         public FormTaoNhomNguoiDung()
         {
             InitializeComponent();
+        }
+
+        private void btn_timChuyenBay_Click(object sender, EventArgs e)
+        {
+            using (var db = new FlightKetDBEntities())
+            {
+                ROLE rOLE = new ROLE();
+                int number = random.Next(100, 999);
+                rOLE.RoleID = "NV" + number.ToString();
+                rOLE.RoleName = tb_nameGroupUser.Text;
+                rOLE.Description = tb_description.Text;
+                rOLE.CanNhapLichChuyenBay = cb_nhapLichChuyenBay.Checked;
+                rOLE.CanBanVe = cb_banVe.Checked;
+                rOLE.CanGhiNhanDatVe = cb_ghiNhanDatVe.Checked;
+                rOLE.CanLapBaoCao = cb_lapBaoCao.Checked;
+                rOLE.CanTraCuuChuyenBay = cb_traCuuChuyenBay.Checked;
+                rOLE.CanThayDoiQuyDinh = cb_thayDoiQuiDinh.Checked;
+
+                db.ROLEs.Add(rOLE);
+                db.SaveChanges();
+                MessageBox.Show("Thêm mới Role thành công!");
+            }
         }
     }
 }
