5889eca2b635ebe1866399d8347d85326ebc999b
21522354 <21522354@gm.uit.edu.vn>
2023-06-07 12:39:13 +0700

    chỉnh sửa chức năng báo cáo tháng

--- flightket/Forms-NhanVien/FormLapBaoCaoThang.cs
+++ flightket/Forms-NhanVien/FormLapBaoCaoThang.cs
@@ -92,6 +92,13 @@ namespace flightket.Forms_NhanVien
 
                     }
 
+                    if(thang == DateTime.Now.Month)
+                    {
+                        MessageBox.Show("Đã cập nhật danh sách doanh thu chuyến bay theo tháng");
+                        lb_doanhThuBanVe.Text = "Doanh thu bán vé tháng " + thang.ToString() + " năm " + nam.ToString();
+                        return;
+                    }
+
                     // thêm dữ liệu mới truy vấn được vào csdl
                     // thêm báo cáo tháng
                     BAOCAOTHANG bAOCAOTHANG1 = new BAOCAOTHANG()
@@ -143,124 +150,21 @@ namespace flightket.Forms_NhanVien
                 {
                     string maBaoCaoThang = baoCaoThang.ToList().First().MaBCThang;
                     var listCTBCThang = db.CT_BCTHANG.Where(a => a.MaBCThang == maBaoCaoThang).ToList();
-                    // Kiểm tra lại nếu tháng đang xét là tháng hiện tại thì phải load lại dữ liệu
-                    if (thang == DateTime.Now.Month)
+                 
+                    // nếu tháng cũ thì chỉ cần load lên
+                    dgv_doanhThuChuyenBay.RowCount = listCTBCThang.Count;
+                    for (int i = 0; i < listCTBCThang.Count; i++)
                     {
-                        // xóa hết chi tiết báo cáo tháng của những chuyến bay trên tháng được chọn
-                        foreach (var cT_BCTHANG in listCTBCThang)
-                        {
-                            CT_BCTHANG cT_BCTHANG1 = db.CT_BCTHANG.Find(cT_BCTHANG.MaBCThang, cT_BCTHANG.MaChuyenBay);
-                            db.CT_BCTHANG.Remove(cT_BCTHANG1);
-                            db.SaveChanges();
-                        }
-
-                        // thao tác truy vấn lại và add lại vào csdl
-                        var listChuyenBay = db.CHUYENBAYs.Where(a => a.NgayGioKhoiHanh.Value.Month == thang && a.NgayGioKhoiHanh.Value.Year == nam).ToList();
-
-                        if (listChuyenBay.Count == 0)
-                        {
-                            MessageBox.Show("Không tìm thấy thông tin các chuyến bay trong tháng");
-                            return;
-                        }
-
-                        double tongDoanhThu = 0;
-                        int tongChuyenbay = listChuyenBay.Count;
-                        double[] doanhThuTungChuyenBay = new double[listChuyenBay.Count];
-                        int[] soVeTungChuyenBay = new int[listChuyenBay.Count];
-                        double[] tileTungChuyenBay = new double[listChuyenBay.Count];
-
-                        // tính doanh thu của từng chuyến bay và cộng vào tổng doanh thu
-                        for (int i = 0; i < listChuyenBay.Count; i++)
-                        {
-                            string maChuyenBay = listChuyenBay[i].MaChuyenBay;
-                            var VeChuyenBay = from cb in db.CHUYENBAYs
-                                              from vcb in db.VECHUYENBAYs
-                                              where cb.MaChuyenBay == vcb.MaChuyenBay && cb.MaChuyenBay == maChuyenBay
-                                              select new { vcb.GiaTien };
-                            var listVeChuyenBay = VeChuyenBay.ToList();
-                            soVeTungChuyenBay[i] = listVeChuyenBay.Count;
-                            foreach (var veChuyenBay in listVeChuyenBay)
-                            {
-                                tongDoanhThu += (double)veChuyenBay.GiaTien;
-                                doanhThuTungChuyenBay[i] += (double)veChuyenBay.GiaTien;
-                            }
-                        }
-                        // tính tỉ lệ doanh thu của từng chuyến bay
-                        for (int i = 0; i < listChuyenBay.Count; i++)
-                        {
-                            tileTungChuyenBay[i] = (doanhThuTungChuyenBay[i] / tongDoanhThu) * 100;
-                            tileTungChuyenBay[i] = Math.Round(tileTungChuyenBay[i], 2);
-                        }
-                        // hiển thị thông tin lên datagridView
-                        dgv_doanhThuChuyenBay.RowCount = listChuyenBay.Count;
-                        for (int i = 0; i < listChuyenBay.Count; i++)
-                        {
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[0].Value = (i + 1).ToString();
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[1].Value = listChuyenBay[i].MaChuyenBay;
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[2].Value = soVeTungChuyenBay[i];
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[3].Value = doanhThuTungChuyenBay[i];
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[4].Value = tileTungChuyenBay[i];
-                            dgv_doanhThuChuyenBay.Rows[i].Height = 30;
-
-                        }
-
-                        // thêm dữ liệu mới truy vấn được vào csdl
-                        // sửa lại báo cáo tháng
-                        string maBaoCaoThang1 = "T" + thang.ToString() + (nam % 100).ToString();
-                        BAOCAOTHANG bAOCAOTHANG1 = db.BAOCAOTHANGs.Find(maBaoCaoThang1);
-                        bAOCAOTHANG1.TongDoanhThuThang = tongDoanhThu;
-                        bAOCAOTHANG1.TongChuyenBay = (short)tongChuyenbay;
-                        db.SaveChanges();
-                        // thêm lại các chi tiết báo cáo tháng
-                        for (int i = 0; i < listChuyenBay.Count; i++)
-                        {
-                            if (tileTungChuyenBay[i] > 0)
-                            {
-                                CT_BCTHANG cT_BCTHANG = new CT_BCTHANG()
-                                {
-                                    MaBCThang = "T" + thang.ToString() + (nam % 100).ToString(),
-                                    MaChuyenBay = listChuyenBay[i].MaChuyenBay,
-                                    SoVe = (short)soVeTungChuyenBay[i],
-                                    DoanhThu = doanhThuTungChuyenBay[i],
-                                    TiLe = tileTungChuyenBay[i]
-                                };
-                                db.CT_BCTHANG.Add(cT_BCTHANG);
-                            }
-                            else
-                            {
-                                CT_BCTHANG cT_BCTHANG = new CT_BCTHANG()
-                                {
-                                    MaBCThang = "T" + thang.ToString() + (nam % 100).ToString(),
-                                    MaChuyenBay = listChuyenBay[i].MaChuyenBay,
-                                    SoVe = (short)soVeTungChuyenBay[i],
-                                    DoanhThu = doanhThuTungChuyenBay[i],
-                                };
-                                db.CT_BCTHANG.Add(cT_BCTHANG);
-                            }
-
-                        }
-                        db.SaveChanges();
-
-                        MessageBox.Show("Đã cập nhật danh sách doanh thu chuyến bay theo tháng");
-                        lb_doanhThuBanVe.Text = "Doanh thu bán vé tháng " + thang.ToString() + " năm " + nam.ToString();
-
+                        dgv_doanhThuChuyenBay.Rows[i].Cells[0].Value = (i + 1).ToString();
+                        dgv_doanhThuChuyenBay.Rows[i].Cells[1].Value = listCTBCThang[i].MaChuyenBay;
+                        dgv_doanhThuChuyenBay.Rows[i].Cells[2].Value = listCTBCThang[i].SoVe;
+                        dgv_doanhThuChuyenBay.Rows[i].Cells[3].Value = listCTBCThang[i].DoanhThu;
+                        dgv_doanhThuChuyenBay.Rows[i].Cells[4].Value = listCTBCThang[i].TiLe;
+                        dgv_doanhThuChuyenBay.Rows[i].Height = 30;
                     }
-                    else
-                    {
-                        // nếu tháng cũ hơn thì chỉ cần load lên
-                        dgv_doanhThuChuyenBay.RowCount = listCTBCThang.Count;
-                        for (int i = 0; i < listCTBCThang.Count; i++)
-                        {
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[0].Value = (i + 1).ToString();
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[1].Value = listCTBCThang[i].MaChuyenBay;
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[2].Value = listCTBCThang[i].SoVe;
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[3].Value = listCTBCThang[i].DoanhThu;
-                            dgv_doanhThuChuyenBay.Rows[i].Cells[4].Value = listCTBCThang[i].TiLe;
-                            dgv_doanhThuChuyenBay.Rows[i].Height = 30;
-                        }
-                        MessageBox.Show("Đã cập nhật danh sách doanh thu chuyến bay theo tháng");
-                        lb_doanhThuBanVe.Text = "Doanh thu bán vé tháng " + thang.ToString() + " năm " + nam.ToString();
-                    }
+                    MessageBox.Show("Đã cập nhật danh sách doanh thu chuyến bay theo tháng");
+                    lb_doanhThuBanVe.Text = "Doanh thu bán vé tháng " + thang.ToString() + " năm " + nam.ToString();
+                    
                     
                 }
                 
