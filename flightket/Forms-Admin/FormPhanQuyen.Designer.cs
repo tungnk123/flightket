df5c9e7eeb5797fd7e4102c5033a4eccb7b2b35a
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-07 10:56:41 +0700

    Add query change Permissions of NhanVien in db

--- flightket/Forms-Admin/FormPhanQuyen.Designer.cs
+++ flightket/Forms-Admin/FormPhanQuyen.Designer.cs
@@ -38,7 +38,7 @@ namespace flightket.Forms_Admin
             this.cb_traCuuChuyenBay = new System.Windows.Forms.CheckBox();
             this.cb_nhapLichChuyenBay = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.cmb_chonLoaiNguoiDung = new System.Windows.Forms.ComboBox();
+            this.cmb_loaiTaiKhoan = new System.Windows.Forms.ComboBox();
             this.btn_xacNhan = new System.Windows.Forms.Button();
             this.lb_title1 = new System.Windows.Forms.Label();
             this.btn_back = new System.Windows.Forms.Button();
@@ -58,7 +58,7 @@ namespace flightket.Forms_Admin
             this.panel_main.Controls.Add(this.cb_traCuuChuyenBay);
             this.panel_main.Controls.Add(this.cb_nhapLichChuyenBay);
             this.panel_main.Controls.Add(this.label1);
-            this.panel_main.Controls.Add(this.cmb_chonLoaiNguoiDung);
+            this.panel_main.Controls.Add(this.cmb_loaiTaiKhoan);
             this.panel_main.Controls.Add(this.btn_xacNhan);
             this.panel_main.Controls.Add(this.lb_title1);
             this.panel_main.Controls.Add(this.btn_back);
@@ -72,68 +72,74 @@ namespace flightket.Forms_Admin
             // cb_thayDoiQuiDinh
             // 
             this.cb_thayDoiQuiDinh.AutoSize = true;
+            this.cb_thayDoiQuiDinh.BackColor = System.Drawing.Color.Transparent;
             this.cb_thayDoiQuiDinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_thayDoiQuiDinh.Location = new System.Drawing.Point(1152, 522);
             this.cb_thayDoiQuiDinh.Name = "cb_thayDoiQuiDinh";
             this.cb_thayDoiQuiDinh.Size = new System.Drawing.Size(278, 36);
             this.cb_thayDoiQuiDinh.TabIndex = 16;
             this.cb_thayDoiQuiDinh.Text = "Thay đổi quy định";
-            this.cb_thayDoiQuiDinh.UseVisualStyleBackColor = true;
+            this.cb_thayDoiQuiDinh.UseVisualStyleBackColor = false;
             // 
             // cb_ghiNhanDatVe
             // 
             this.cb_ghiNhanDatVe.AutoSize = true;
+            this.cb_ghiNhanDatVe.BackColor = System.Drawing.Color.Transparent;
             this.cb_ghiNhanDatVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_ghiNhanDatVe.Location = new System.Drawing.Point(412, 522);
             this.cb_ghiNhanDatVe.Name = "cb_ghiNhanDatVe";
             this.cb_ghiNhanDatVe.Size = new System.Drawing.Size(252, 36);
             this.cb_ghiNhanDatVe.TabIndex = 15;
             this.cb_ghiNhanDatVe.Text = "Ghi nhận đặt vé";
-            this.cb_ghiNhanDatVe.UseVisualStyleBackColor = true;
+            this.cb_ghiNhanDatVe.UseVisualStyleBackColor = false;
             // 
             // cb_lapBaoCao
             // 
             this.cb_lapBaoCao.AutoSize = true;
+            this.cb_lapBaoCao.BackColor = System.Drawing.Color.Transparent;
             this.cb_lapBaoCao.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_lapBaoCao.Location = new System.Drawing.Point(1152, 390);
             this.cb_lapBaoCao.Name = "cb_lapBaoCao";
             this.cb_lapBaoCao.Size = new System.Drawing.Size(204, 36);
             this.cb_lapBaoCao.TabIndex = 14;
             this.cb_lapBaoCao.Text = "Lập báo cáo";
-            this.cb_lapBaoCao.UseVisualStyleBackColor = true;
+            this.cb_lapBaoCao.UseVisualStyleBackColor = false;
             // 
             // cb_banVe
             // 
             this.cb_banVe.AutoSize = true;
+            this.cb_banVe.BackColor = System.Drawing.Color.Transparent;
             this.cb_banVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_banVe.Location = new System.Drawing.Point(412, 390);
             this.cb_banVe.Name = "cb_banVe";
             this.cb_banVe.Size = new System.Drawing.Size(131, 36);
             this.cb_banVe.TabIndex = 13;
             this.cb_banVe.Text = "Bán vé";
-            this.cb_banVe.UseVisualStyleBackColor = true;
+            this.cb_banVe.UseVisualStyleBackColor = false;
             // 
             // cb_traCuuChuyenBay
             // 
             this.cb_traCuuChuyenBay.AutoSize = true;
+            this.cb_traCuuChuyenBay.BackColor = System.Drawing.Color.Transparent;
             this.cb_traCuuChuyenBay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_traCuuChuyenBay.Location = new System.Drawing.Point(1152, 271);
             this.cb_traCuuChuyenBay.Name = "cb_traCuuChuyenBay";
             this.cb_traCuuChuyenBay.Size = new System.Drawing.Size(302, 36);
             this.cb_traCuuChuyenBay.TabIndex = 12;
             this.cb_traCuuChuyenBay.Text = "Tra cứu chuyến bay";
-            this.cb_traCuuChuyenBay.UseVisualStyleBackColor = true;
+            this.cb_traCuuChuyenBay.UseVisualStyleBackColor = false;
             // 
             // cb_nhapLichChuyenBay
             // 
             this.cb_nhapLichChuyenBay.AutoSize = true;
+            this.cb_nhapLichChuyenBay.BackColor = System.Drawing.Color.Transparent;
             this.cb_nhapLichChuyenBay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_nhapLichChuyenBay.Location = new System.Drawing.Point(412, 271);
             this.cb_nhapLichChuyenBay.Name = "cb_nhapLichChuyenBay";
             this.cb_nhapLichChuyenBay.Size = new System.Drawing.Size(328, 36);
             this.cb_nhapLichChuyenBay.TabIndex = 11;
             this.cb_nhapLichChuyenBay.Text = "Nhập lịch chuyến nay";
-            this.cb_nhapLichChuyenBay.UseVisualStyleBackColor = true;
+            this.cb_nhapLichChuyenBay.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
@@ -141,18 +147,19 @@ namespace flightket.Forms_Admin
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(622, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 26);
+            this.label1.Size = new System.Drawing.Size(217, 26);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Chọn loại người dùng";
+            this.label1.Text = "Chọn loại tài khoản";
             // 
-            // cmb_chonLoaiNguoiDung
+            // cmb_loaiTaiKhoan
             // 
-            this.cmb_chonLoaiNguoiDung.BackColor = System.Drawing.Color.Silver;
-            this.cmb_chonLoaiNguoiDung.FormattingEnabled = true;
-            this.cmb_chonLoaiNguoiDung.Location = new System.Drawing.Point(967, 122);
-            this.cmb_chonLoaiNguoiDung.Name = "cmb_chonLoaiNguoiDung";
-            this.cmb_chonLoaiNguoiDung.Size = new System.Drawing.Size(279, 34);
-            this.cmb_chonLoaiNguoiDung.TabIndex = 9;
+            this.cmb_loaiTaiKhoan.BackColor = System.Drawing.Color.Silver;
+            this.cmb_loaiTaiKhoan.FormattingEnabled = true;
+            this.cmb_loaiTaiKhoan.Location = new System.Drawing.Point(967, 122);
+            this.cmb_loaiTaiKhoan.Name = "cmb_loaiTaiKhoan";
+            this.cmb_loaiTaiKhoan.Size = new System.Drawing.Size(279, 34);
+            this.cmb_loaiTaiKhoan.TabIndex = 9;
+            this.cmb_loaiTaiKhoan.SelectedIndexChanged += new System.EventHandler(this.cmb_loaiTaiKhoan_SelectedIndexChanged);
             // 
             // btn_xacNhan
             // 
@@ -165,6 +172,7 @@ namespace flightket.Forms_Admin
             this.btn_xacNhan.TabIndex = 8;
             this.btn_xacNhan.Text = "Xác nhận";
             this.btn_xacNhan.UseVisualStyleBackColor = false;
+            this.btn_xacNhan.Click += new System.EventHandler(this.btn_xacNhan_Click);
             // 
             // lb_title1
             // 
@@ -187,6 +195,7 @@ namespace flightket.Forms_Admin
             this.btn_back.Size = new System.Drawing.Size(70, 70);
             this.btn_back.TabIndex = 0;
             this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
             // 
             // btn_home
             // 
@@ -196,6 +205,7 @@ namespace flightket.Forms_Admin
             this.btn_home.Size = new System.Drawing.Size(70, 70);
             this.btn_home.TabIndex = 1;
             this.btn_home.UseVisualStyleBackColor = true;
+            this.btn_home.Click += new System.EventHandler(this.btn_home_Click);
             // 
             // FormPhanQuyen
             // 
@@ -210,6 +220,7 @@ namespace flightket.Forms_Admin
             this.Name = "FormPhanQuyen";
             this.Text = "Flightket - Admin - Phân quyền";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormPhanQuyen_FormClosed);
             this.Load += new System.EventHandler(this.FormPhanQuyen_Load);
             this.panel_main.ResumeLayout(false);
             this.panel_main.PerformLayout();
@@ -227,7 +238,7 @@ namespace flightket.Forms_Admin
         private System.Windows.Forms.CheckBox cb_traCuuChuyenBay;
         private System.Windows.Forms.CheckBox cb_nhapLichChuyenBay;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cmb_chonLoaiNguoiDung;
+        private System.Windows.Forms.ComboBox cmb_loaiTaiKhoan;
         private System.Windows.Forms.Button btn_xacNhan;
         private System.Windows.Forms.Label lb_title1;
         private System.Windows.Forms.Button btn_back;
