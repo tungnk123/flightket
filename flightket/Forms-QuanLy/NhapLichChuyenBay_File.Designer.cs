e712c03f5c21c0e580e1cd91f0d30afed106b3ef
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-06-08 09:51:37 +0700

    cmt

--- flightket/Forms-QuanLy/NhapLichChuyenBay_File.Designer.cs
+++ flightket/Forms-QuanLy/NhapLichChuyenBay_File.Designer.cs
@@ -54,9 +54,10 @@
             this.panel1.Controls.Add(this.btn_home);
             this.panel1.Controls.Add(this.btn_back);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(38, 298);
+            this.panel1.Location = new System.Drawing.Point(75, 468);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1823, 482);
+            this.panel1.Size = new System.Drawing.Size(2418, 742);
             this.panel1.TabIndex = 0;
             // 
             // panel_xacNhan
@@ -64,9 +65,10 @@
             this.panel_xacNhan.Controls.Add(this.lb_fileDaChon);
             this.panel_xacNhan.Controls.Add(this.btn_xacNhan);
             this.panel_xacNhan.Controls.Add(this.label3);
-            this.panel_xacNhan.Location = new System.Drawing.Point(259, 251);
+            this.panel_xacNhan.Location = new System.Drawing.Point(199, 389);
+            this.panel_xacNhan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel_xacNhan.Name = "panel_xacNhan";
-            this.panel_xacNhan.Size = new System.Drawing.Size(1343, 217);
+            this.panel_xacNhan.Size = new System.Drawing.Size(2014, 334);
             this.panel_xacNhan.TabIndex = 21;
             // 
             // lb_fileDaChon
@@ -76,10 +78,11 @@
             this.lb_fileDaChon.Font = new System.Drawing.Font("Inter", 20.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_fileDaChon.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.lb_fileDaChon.LinkColor = System.Drawing.Color.Black;
-            this.lb_fileDaChon.Location = new System.Drawing.Point(499, 13);
+            this.lb_fileDaChon.Location = new System.Drawing.Point(748, 20);
+            this.lb_fileDaChon.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_fileDaChon.Name = "lb_fileDaChon";
-            this.lb_fileDaChon.Padding = new System.Windows.Forms.Padding(10);
-            this.lb_fileDaChon.Size = new System.Drawing.Size(695, 77);
+            this.lb_fileDaChon.Padding = new System.Windows.Forms.Padding(15, 15, 15, 15);
+            this.lb_fileDaChon.Size = new System.Drawing.Size(1042, 118);
             this.lb_fileDaChon.TabIndex = 23;
             this.lb_fileDaChon.TabStop = true;
             this.lb_fileDaChon.Text = "C://document?fighter/import.csv";
@@ -90,9 +93,10 @@
             this.btn_xacNhan.BackColor = System.Drawing.Color.PaleTurquoise;
             this.btn_xacNhan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btn_xacNhan.Font = new System.Drawing.Font("Inter", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_xacNhan.Location = new System.Drawing.Point(608, 148);
+            this.btn_xacNhan.Location = new System.Drawing.Point(912, 228);
+            this.btn_xacNhan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_xacNhan.Name = "btn_xacNhan";
-            this.btn_xacNhan.Size = new System.Drawing.Size(132, 43);
+            this.btn_xacNhan.Size = new System.Drawing.Size(198, 66);
             this.btn_xacNhan.TabIndex = 22;
             this.btn_xacNhan.Text = "Xác nhận";
             this.btn_xacNhan.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -104,9 +108,10 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Inter", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(282, 33);
+            this.label3.Location = new System.Drawing.Point(423, 51);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(196, 35);
+            this.label3.Size = new System.Drawing.Size(298, 54);
             this.label3.TabIndex = 21;
             this.label3.Text = "File đã chọn :";
             // 
@@ -118,10 +123,11 @@
             this.llb_chonFile.Image = global::flightket.Properties.Resources.ic_upload;
             this.llb_chonFile.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.llb_chonFile.LinkColor = System.Drawing.Color.Black;
-            this.llb_chonFile.Location = new System.Drawing.Point(704, 162);
+            this.llb_chonFile.Location = new System.Drawing.Point(889, 248);
+            this.llb_chonFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.llb_chonFile.Name = "llb_chonFile";
-            this.llb_chonFile.Padding = new System.Windows.Forms.Padding(10);
-            this.llb_chonFile.Size = new System.Drawing.Size(455, 77);
+            this.llb_chonFile.Padding = new System.Windows.Forms.Padding(15, 15, 15, 15);
+            this.llb_chonFile.Size = new System.Drawing.Size(682, 118);
             this.llb_chonFile.TabIndex = 20;
             this.llb_chonFile.TabStop = true;
             this.llb_chonFile.Text = "Chọn từ máy tính của bạn";
@@ -133,9 +139,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Inter", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(834, 105);
+            this.label2.Location = new System.Drawing.Point(1084, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(269, 35);
+            this.label2.Size = new System.Drawing.Size(411, 54);
             this.label2.TabIndex = 17;
             this.label2.Text = "Chọn file cần nhập";
             // 
@@ -143,9 +150,10 @@
             // 
             this.btn_home.BackgroundImage = global::flightket.Properties.Resources.ic_home;
             this.btn_home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_home.Location = new System.Drawing.Point(342, 28);
+            this.btn_home.Location = new System.Drawing.Point(359, 42);
+            this.btn_home.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_home.Name = "btn_home";
-            this.btn_home.Size = new System.Drawing.Size(59, 51);
+            this.btn_home.Size = new System.Drawing.Size(88, 78);
             this.btn_home.TabIndex = 15;
             this.btn_home.UseVisualStyleBackColor = true;
             this.btn_home.Click += new System.EventHandler(this.btn_home_Click);
@@ -154,9 +162,10 @@
             // 
             this.btn_back.BackgroundImage = global::flightket.Properties.Resources.ic_back;
             this.btn_back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_back.Location = new System.Drawing.Point(277, 28);
+            this.btn_back.Location = new System.Drawing.Point(261, 42);
+            this.btn_back.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_back.Name = "btn_back";
-            this.btn_back.Size = new System.Drawing.Size(59, 51);
+            this.btn_back.Size = new System.Drawing.Size(88, 78);
             this.btn_back.TabIndex = 16;
             this.btn_back.UseVisualStyleBackColor = true;
             this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
@@ -166,9 +175,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Inter", 39.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(638, 28);
+            this.label1.Location = new System.Drawing.Point(790, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(605, 64);
+            this.label1.Size = new System.Drawing.Size(917, 97);
             this.label1.TabIndex = 13;
             this.label1.Text = "Nhập Lịch Chuyến Bay";
             // 
@@ -177,9 +187,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::flightket.Properties.Resources._2_removebg_preview__1__1;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(1406, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(2080, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(486, 126);
+            this.pictureBox1.Size = new System.Drawing.Size(729, 194);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
@@ -188,28 +199,31 @@
             this.lb_progress.BackColor = System.Drawing.Color.Transparent;
             this.lb_progress.Font = new System.Drawing.Font("Inter", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_progress.ForeColor = System.Drawing.Color.Red;
-            this.lb_progress.Location = new System.Drawing.Point(1592, 1012);
+            this.lb_progress.Location = new System.Drawing.Point(2388, 1557);
+            this.lb_progress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_progress.Name = "lb_progress";
-            this.lb_progress.Size = new System.Drawing.Size(269, 35);
+            this.lb_progress.Size = new System.Drawing.Size(404, 54);
             this.lb_progress.TabIndex = 18;
             this.lb_progress.Text = "Tiến Trình";
             this.lb_progress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // NhapLichChuyenBay_File
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::flightket.Properties.Resources._350248398_786598096329880_2111811280162483567_n__1_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(2586, 1466);
             this.Controls.Add(this.lb_progress);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "NhapLichChuyenBay_File";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NhapLichChuyenBay_File";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel_xacNhan.ResumeLayout(false);
